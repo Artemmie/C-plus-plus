0128ae6b1eadb6455d0531c47993f17998e1793a
Art <artemgerasimov55@gmail.com>
2018-02-01 17:54:51 +0200

    Added some new code to main.cpp

--- Section_02/CowBullGame/main.cpp
+++ Section_02/CowBullGame/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+void printIntro();
+void getGuess();
+
 int main() {
-	cout << "Welcome to Bulls and Cows!\n";
+	printIntro();
+	getGuess();
 	return 0;
+}
+
+void printIntro() {
+	constexpr int WORLD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows!\n";
+	cout << "Can you guess the " << WORLD_LENGTH << " letter isogram I am thinking of?\n";
+}
+void getGuess() {
+	cout << "Enter your Guess: ";
+	string  guess = "";
+	getline(cin, guess);
+	cout << "Your guess is: " << guess << "\n";
 }
