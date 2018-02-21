540216fa66f07b2166a356d54239e58b3e233d14
Art <artemgerasimov55@gmail.com>
2018-02-22 00:44:18 +0200

    Fixed positionReport to report the right game object

--- Escape_Room/Source/Escape_Room/PositionReport.cpp
+++ Escape_Room/Source/Escape_Room/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -10,7 +10,6 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -18,8 +17,8 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	FString objectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report on %s"), *objectName);
 	
 }
 
